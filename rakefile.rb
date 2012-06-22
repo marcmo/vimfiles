5a4310df8cfbed032d524f93bdac6c592f44778f
muellero <oliver.mueller@esrlabs.com>
2012-06-22 17:15:41 +0200

    on the way to rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,13 +1,27 @@
-
-Plugins = Dir.glob("bundle/*").each do |b|
-  b.gsub!(/bundle\//,'')
+def windows?
+  (RUBY_PLATFORM =~ /cygwin|mswin|mingw|bccwin|wince|emx/) != nil
+end
+def mac?
+  (RUBY_PLATFORM =~ /darwin/) != nil
+end
+def unix?
+  !windows?
+end
+def linux?
+  unix? and not mac?
 end
 
-desc "update plugins: [#{Plugins.join(",")}]"
-task :update do
-  Plugins.each do |p|
-    cd "bundle/#{p}" do
-      sh "git pull"
-    end
-  end
+
+task :vundle do
+  sh "git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle"
+end
+
+desc "install plugins"
+task :install => :vundle do
+  sh "vim +BundleInstall +qall"
+end
+
+desc "link"
+task :link do
+  sh 'echo -e "source ~/.vim/_vimrc_universal\\nsource ~/.vim/_vimrc_linux" > ~/.vimrc2'
 end
